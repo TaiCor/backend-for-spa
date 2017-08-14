7fbcdb1b6e2bbc9b3ecff44623dc2ccb1d47b8ae
Vadim Kapustin <vkapustin@msteam.local>
2017-08-14 16:32:57 +0700

    fix

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.get('/posts', function (req, res) {
 app.get('/comments', function (req, res) {
 })
 
-app.get('/rate', function () {
+app.get('/rate', function (req, res) {
 })
 
-app.delete('/deletePost', function () {
+app.delete('/deletePost', function (req, res) {
 })
 
 app.put('/upgradePost', function (req, res) {
