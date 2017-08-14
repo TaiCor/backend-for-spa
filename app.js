8ebe7b753061716dfbc7d39944581cfd85bef80a
Vadim Kapustin <vkapustin@msteam.local>
2017-08-14 16:32:17 +0700

    add methods

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+app.get('/posts', function (req, res) {
+})
+
+app.get('/comments', function (req, res) {
+})
+
+app.get('/rate', function () {
+})
+
+app.delete('/deletePost', function () {
+})
+
+app.put('/upgradePost', function (req, res) {
+})
+
+app.post('/login', function (req, res) {})
+
+app.post('/singup', function (req, res) {})
