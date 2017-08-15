06392f8fdd5a5e64f36eaa9ea4bb96fa818ecc91
Vadim Kapustin <vkapustin@msteam.local>
2017-08-15 18:19:00 +0700

    fix bugs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: '5000',
@@ -8,6 +9,7 @@ const connection = mysql.createConnection({
   password: 'vandal123',
   database: 'mydb'
 })
+
 app.use('/images', express.static('public'))
 
 connection.connect(function (err) {
@@ -19,19 +21,19 @@ connection.connect(function (err) {
 })
 
 app.get('/posts', function (req, res) {
-  connection.query('select posts.id, posts.picture, posts.title, posts.description, posts.date, users.name from posts left join users on posts.users_id = users.id', function (err, rows) {
+  connection.query('select posts.id, posts.picture, posts.title, posts.description, posts.date, users.name from posts left join users on posts.user_id = users.id', function (err, rows) {
     connection.end()
     if (!err) {
       res.json(rows)
     } else {
-      console.log('error')
+      console.log(err)
     }
   })
 })
 
 app.get('/myposts', function (req, res) {
   let userId
-  connection.query('select Posts.id, Posts.picture, Posts.title, Posts.description, Posts.date, Users.Name from Posts left join Users on Posts.Users_id = Users.id where Users.id =' + mysql.escape(userId), function (err, rows) {
+  connection.query('select posts.id, posts.picture, posts.title, posts.description, posts.date, users.name from posts left join users on posts.Users_id = Users.id where Users.id =' + mysql.escape(userId), function (err, rows) {
     connection.end()
     if (!err) {
       res.json(rows)
