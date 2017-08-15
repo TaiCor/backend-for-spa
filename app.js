e7d92c6d7fafc8010abdd5c9d78d429c1f1f4418
Vadim Kapustin <vkapustin@msteam.local>
2017-08-15 14:24:51 +0700

    d

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mydb'
+})
+app.use(express.static('public'))
+
+connection.connect(function (err) {
+  if (!err) {
+    console.log('DB connected! ...')
+  } else {
+    console.log('error connecting to DB ...')
+  }
+})
 
 app.get('/posts', function (req, res) {
+  connection.query('select Posts.id, Posts.picture, Posts.title, Posts.description, Posts.date, Users.Name from Posts left join Users on Posts.Users_id = Users.id', function (err, rows) {
+    connection.end()
+    if (!err) {
+      res.json(rows)
+    } else {
+      console.log('error')
+    }
+  })
+})
+
+app.get('/myposts', function (req, res) {
+  let userId
+  connection.query('select Posts.id, Posts.picture, Posts.title, Posts.description, Posts.date, Users.Name from Posts left join Users on Posts.Users_id = Users.id where Users.id =' + mysql.escape(userId), function (err, rows) {
+    connection.end()
+    if (!err) {
+      res.json(rows)
+    } else {
+      console.log('error')
+    }
+  })
 })
 
 app.get('/comments', function (req, res) {
@@ -10,7 +46,7 @@ app.get('/comments', function (req, res) {
 app.get('/rate', function (req, res) {
 })
 
-app.delete('/deletePost', function (req, res) {
+app.delete('/deletePost/:id', function (req, res) {
 })
 
 app.put('/upgradePost', function (req, res) {
