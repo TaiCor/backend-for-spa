c71649f5a34de1830c3065ac75bcf3e2b49a70b5
Vadim Kapustin <vkapustin@msteam.local>
2017-08-16 17:36:49 +0700

    meh

--- app.js
+++ app.js
@@ -34,17 +34,16 @@ app.get('/posts', function (req, res) {
   })
 })
 
-app.get('/myposts', function (req, res) {
-  let userId
-  connection.query('select posts.id, posts.picture, posts.title, posts.description, posts.date, users.name from posts left join users on posts.Users_id = Users.id where Users.id =' + mysql.escape(userId), function (err, rows) {
-    connection.end()
-    if (!err) {
-      res.json(rows)
-    } else {
-      console.log(err)
-    }
-  })
-})
+// app.get('/myposts', function (req, res) {
+//   connection.query('select posts.id, posts.picture, posts.title, posts.description, posts.date, users.name from posts left join users on posts.Users_id = Users.id where Users.id =' + mysql.escape(userId), function (err, rows) {
+//     connection.end()
+//     if (!err) {
+//       res.json(rows)
+//     } else {
+//       console.log(err)
+//     }
+//   })
+// })
 
 app.get('/comments', function (req, res) {
   connection.query(`select comments.text, comments.date, comments.post_id, users.name from comments left join users on users.id = comments.user_id `, function (err, rows) {
@@ -61,6 +60,13 @@ app.get('/rate', function (req, res) {
 })
 
 app.delete('/deletePost/:id', function (req, res) {
+  connection.query('delete from posts where id=' + mysql.escape(req.params.id), function (err, rows) {
+    if (!err) {
+      console.log('row deleted!')
+    } else {
+      console.log(err)
+    }
+  })
 })
 
 app.put('/upgradePost', function (req, res) {
@@ -68,7 +74,17 @@ app.put('/upgradePost', function (req, res) {
 
 app.post('/login', function (req, res) {})
 
-app.post('/singup', function (req, res) {})
+app.post('/singup/', function (req, res) {
+  res.send(req.params)
+})
+//   connection.query('insert into users set ?', [user], function (err, rows) {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       console.log('user added')
+//     }
+//   })
+// })
 
 app.listen(4000, function () {
   console.log('listen 4000')
